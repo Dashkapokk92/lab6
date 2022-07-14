b2ecf42acaf93287be20b8b7d832fc69cbc57163
Dashkapokk92 <danem2@mail.ru>
2022-07-14 20:54:56 +0300

    initial commit

--- src/main/java/ru/netology/statistic/StatsService.java
+++ src/main/java/ru/netology/statistic/StatsService.java
@@ -1,70 +1,70 @@
 package ru.netology.statistic;
 
 
-    public class StatsService {
-        public int calculateSum(int[] sales) {
-            int sum = 0;
-            for (int sale : sales) {
-                sum += sale;
-            }
-            return sum;
+public class StatsService {
+    public int calculateSum(int[] sales) {
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
         }
+        return sum;
+    }
 
-        public int calculateMean(int[] sales) {
+    public int calculateMean(int[] sales) {
 
-            return calculateSum(sales) / sales.length;
-        }
+        return calculateSum(sales) / sales.length;
+    }
 
-        public int findMaxSale(int[] sales) {
-            int maxMonth = 0;
-            int month = 0;
-            for (int sale : sales) {
-                if (sale >= sales[maxMonth]) {
-                    maxMonth = month;
-                }
+    public int findMaxSale(int[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (int sale : sales) {
+            if (sale >= sales[maxMonth]) {
                 maxMonth = month;
             }
-            month = month + 1;
-            return maxMonth + 1;
+            maxMonth = month;
         }
+        month = month + 1;
+        return maxMonth + 1;
+    }
 
-        public int findMinSale(int[] sales) {
-            int minMonth = 0;
-            int month = 0;
-            for (int sale : sales) {
-                if (sale <= sales[minMonth]) {
-                    minMonth = month;
-                }
+    public int findMinSale(int[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (int sale : sales) {
+            if (sale <= sales[minMonth]) {
                 minMonth = month;
             }
-            month = month + 1;
-            return minMonth + 1;
+            minMonth = month;
         }
-
-        public int findMaxMeanSale(int[] sales) {
-
-            int avg = calculateMean(sales);
-            int month = 0;
-            for (long sale : sales) {
-                if (sale >= avg) {
-                    month++;
-                }
-            }
-            return month;
-        }
-
-        public int findMinMeanSale(int[] sales) {
-
-            int avg = calculateMean(sales);
-            int month = 0;
-            for (long sale : sales) {
-                if (sale < avg) {
-                    month++;
-                }
-            }
-            return month;
-        }
-
-
+        month = month + 1;
+        return minMonth + 1;
     }
+
+    public int findMaxMeanSale(int[] sales) {
+
+        int avg = calculateMean(sales);
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= avg) {
+                month++;
+            }
+        }
+        return month;
+    }
+
+    public int findMinMeanSale(int[] sales) {
+
+        int avg = calculateMean(sales);
+        int month = 0;
+        for (long sale : sales) {
+            if (sale < avg) {
+                month++;
+            }
+        }
+        return month;
+    }
+
+
+}
 
